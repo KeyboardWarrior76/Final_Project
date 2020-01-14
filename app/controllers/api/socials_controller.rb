ebe8006d12a9143f8f93714cc2213453270a7540
Mason Eyre <mason.deyre@gmail.com>
2020-01-13 20:40:33 -0700

    missing controllers

---
+++ app/controllers/api/socials_controller.rb
@@ -0,0 +1,41 @@
+class Api::SocialsController < ApplicationController
+
+  before_action :set_project
+
+  def index
+    render json: @project.socials
+  end
+
+  def create
+    @social = @project.socials.new(social_params)
+    if @social.save
+      render json: @social
+    else
+      render json: {errors: @social.errors}, status: :unproccessable_entity
+    end
+  end
+
+  def update
+    @social = @project.socials.find(params[:id])
+    if @social.update(social_params)
+      render json: @social
+    else
+      render json: {errors: @social.errors}, status: :unproccessable_entity
+    end
+  end
+
+  def destroy
+    @project.socials.find(params[:id]).destroy
+    render json: {message: 'social has been wrecked, bro'}
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def social_params
+    params.require(:social).permit(:facebook_graph, :sharing, :forums, :messaging)
+  end
+end
