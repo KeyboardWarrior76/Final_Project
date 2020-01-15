5d863a9627e9dbcf2b713ae0d09ea520545d9e4d
Oz <ozzo3@yahoo.com>
2020-01-14 23:59:24 -0700

    Dependent destroy added, and relationships defined

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,9 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_many :billings
-  has_many :user_contents
-  has_many :securities
+  has_many :billings, dependent: :destroy
+  has_many :user_contents, dependent: :destroy
+  has_many :securities, dependent: :destroy
+  has_many :socials, dependent: :destroy
+  has_many :apps, dependent: :destroy
+  has_many :analytics, dependent: :destroy
 end
