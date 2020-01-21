d9d16e486dbbf3be6043c69a9898cff8355b971c
Benjamin Hansen <bhans23@gmail.com>
2020-01-20 20:47:11 -0700

    added funtionality to profile page

--- client/src/components/shared/Profile.js
+++ client/src/components/shared/Profile.js
@@ -6,33 +6,55 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
+import EmailIcon from '@material-ui/icons/Email';
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import Container from '@material-ui/core/Container';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginTop: 100,
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
 
 const Profile = ({ auth }) =>
   (
-    <div>
-      <List>
-        {auth.authenticated && (
-          <ListItem>
-            <ListItemText primary={` ${auth.user.email}`}/>           
-          </ListItem>
-        )}
-         {auth.authenticated && (
-          <ListItem>
-            <ListItemText primary={` ${auth.user.first_name}`}/>
-          </ListItem>
-         )}
-         {auth.authenticated && (
-          <ListItem>
-            <ListItemText primary={` ${auth.user.last_name}`}/>
-          </ListItem>
-         )}
+    <Container maxWidth="sm" className={useStyles.root}>
+    <div >
+      <Card>
+        <CardContent>
+        <List>
+        {  auth.authenticated && (
+            <ListItem>
+              <ListItemIcon>
+                <EmailIcon/>
+              </ListItemIcon>
+              <ListItemText primary={` ${auth.user.email}`}/>           
+            </ListItem>
+          )}
+           {auth.authenticated && (
+            <ListItem>
+              <ListItemIcon>
+                <EmojiPeopleIcon/>
+              </ListItemIcon>
+              <ListItemText primary={` ${auth.user.first_name} ${auth.user.last_name}`} />
+            </ListItem>
+           )}
+          
           
         
-      </List>
+          </List>
+         </CardContent>
+        </Card>
+      
     </div>
+  </Container>
   )
 
 const ConnectedProfile = () =>
