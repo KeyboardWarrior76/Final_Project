9e5b99ed87d367cc686838cef514125ba2da8932
Mason Eyre <mason.deyre@gmail.com>
2020-01-22 20:33:10 -0700

    changed defaults of app model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_16_025713) do
+ActiveRecord::Schema.define(version: 2020_01_23_033058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,8 +48,8 @@ ActiveRecord::Schema.define(version: 2020_01_16_025713) do
   end
 
   create_table "apps", force: :cascade do |t|
-    t.string "ui_level"
-    t.string "size"
+    t.string "ui_level", default: "simple"
+    t.string "size", default: "small"
     t.bigint "project_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
