28c8e6c74ed6ca5fc7d676ec5d70de470d536ba1
Chris Anderson <chris.anderson955@gmail.com>
2020-01-23 18:20:21 -0700

    removed binding.pry from Added project mailer

--- app/mailers/project_sender_mailer.rb
+++ app/mailers/project_sender_mailer.rb
@@ -13,7 +13,6 @@ class ProjectSenderMailer < ApplicationMailer
 		content = Content.new(type: 'text/plain', value: 'and easy to do
 		anywhere, even with Ruby')
 		mail = Mail.new(from, subject, to, content)
-		binding.pry
 		sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
 		response = sg.client.mail._('send').post(request_body: mail.to_json)
 
