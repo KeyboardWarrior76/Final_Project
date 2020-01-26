e0b31c914508533dcea0c148053cea050fc55acc
Mason Eyre <mason.deyre@gmail.com>
2020-01-25 22:07:33 -0700

    Cleaned up AccountForm
    organized the code a bit

--- client/src/components/forms/AccountForm.js
+++ client/src/components/forms/AccountForm.js
@@ -30,64 +30,75 @@ class AccountForm extends Component {
       <div align='center'>
         <h3><p>Users & Accounts</p></h3>
         <Fab variant="extended" color={one}
-          onClick={() => {toggleCategoryItem('account', 'email_pass'); this.toggle(one, 'one');} }
-        >
+          onClick={() => {
+            toggleCategoryItem('account', 'email_pass');
+            this.toggle(one, 'one');
+          }}>
           Email/Password Login
         </Fab>
         <Fab variant="extended" color={two}
-          onChange={() => this.toggleActive()}
-          onClick={() => {toggleCategoryItem('account', 'facebook'); this.toggle(two, 'two');} }
-        >
+          onClick={() => {
+            toggleCategoryItem('account', 'facebook');
+            this.toggle(two, 'two');
+          }}>
           Facebook Login
         </Fab>
         <Fab variant="extended" color={three}
-          onChange={() => this.toggleActive()}
-          onClick={() => {toggleCategoryItem('account', 'twitter'); this.toggle(three, 'three');} }
-        >
+          onClick={() => {
+            toggleCategoryItem('account', 'twitter');
+            this.toggle(three, 'three');
+          }}>
           Twitter Login
         </Fab>
         <br/>
         <Fab variant="extended" color={four}
-          onChange={() => this.toggleActive()}
-          onClick={() => {toggleCategoryItem('account', 'google'); this.toggle(four, 'four');} }
-        >
+          onClick={() => {
+            toggleCategoryItem('account', 'google');
+            this.toggle(four, 'four');
+          }}>
           Google Login
         </Fab>
         <Fab variant="extended" color={five}
-          onChange={() => this.toggleActive()}
-          onClick={() => {toggleCategoryItem('account', 'linkedin'); this.toggle(five, 'five');} }
-        >
+          onClick={() => {
+            toggleCategoryItem('account', 'linkedin');
+            this.toggle(five, 'five');
+          }}>
           Linkedin Login
         </Fab>
         <Fab variant="extended" color={six}
-          onChange={() => this.toggleActive()}
-          onClick={() => {toggleCategoryItem('account', 'github'); this.toggle(six, 'six');} }
-        >
+          onClick={() => {
+            toggleCategoryItem('account', 'github');
+            this.toggle(six, 'six');
+          }}>
           GitHub Login
         </Fab>
         <Fab variant="extended" color={seven}
-          onChange={() => this.toggleActive()}
-          onClick={() => {toggleCategoryItem('account', 'invitation'); this.toggle(seven, 'seven');} }
-        >
+          onClick={() => {
+            toggleCategoryItem('account', 'invitation');
+            this.toggle(seven, 'seven');
+          }}>
           User Invitation Emails
         </Fab>
         <br/>
         <Fab variant="extended" color={eight}
-          onChange={() => this.toggleActive()}
-          onClick={() => {toggleCategoryItem('account', 'multi_account'); this.toggle(eight, 'eight');} }
-        >
+          onClick={() => {
+            toggleCategoryItem('account', 'multi_account');
+            this.toggle(eight, 'eight');
+          }}>
           Multi-tenant Accounts
         </Fab>
         <Fab variant="extended" color={nine}
-          onChange={() => this.toggleActive()}
-          onClick={() => {toggleCategoryItem('account', 'subdomain'); this.toggle(nine, 'nine');} }
-        >
+          onClick={() => {
+            toggleCategoryItem('account', 'subdomain');
+            this.toggle(nine, 'nine');
+          }}>
           Subdomains
         </Fab>
         <Fab variant="extended" color={ten}
-          onChange={() => this.toggleActive()}
-          onClick={() => {toggleCategoryItem('account', 'custom'); this.toggle(ten, 'ten');} }
-        >
+          onClick={() => {
+            toggleCategoryItem('account', 'custom');
+            this.toggle(ten, 'ten');
+          }}>
           Custom Domains
         </Fab>
       </div>
