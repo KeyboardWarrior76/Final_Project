7c3732cbd1cf81bfaf1f0d4dec799840a953f078
Chris Anderson <chris.anderson955@gmail.com>
2020-01-27 19:44:11 -0700

    in the middle of styling

--- client/src/components/forms/AccountForm.js
+++ client/src/components/forms/AccountForm.js
@@ -1,23 +1,22 @@
 import React, {Component} from 'react';
-import Fab from '@material-ui/core/Fab';
 import { ProjectConsumer, } from "../../providers/ProjectProvider";
 
 class AccountForm extends Component {
 
   state = { button: {
-    one: 'default', two: 'default', three: 'default',
-    four: 'default', five: 'default', six: 'default',
-    seven: 'default', eight: 'default', nine: 'default', ten: 'default'
+    one: 'estimatorButton', two: 'estimatorButton', three: 'estimatorButton',
+    four: 'estimatorButton', five: 'estimatorButton', six: 'estimatorButton',
+    seven: 'estimatorButton', eight: 'estimatorButton', nine: 'estimatorButton', ten: 'estimatorButton'
     }
   }
 
   toggle = (unique, item) => {
     const{ button } = this.state
-    if (unique === 'default') {
-      this.setState({ button: {...button, [item]: 'primary'} })
+    if (unique === 'estimatorButton') {
+      this.setState({ button: {...button, [item]: 'selectedButton'} })
     }
-    else if (unique === 'primary') {
-      this.setState({ button: {...button, [item]: 'default'} })
+    else if (unique === 'selectedButton') {
+      this.setState({ button: {...button, [item]: 'estimatorButton'} })
     }
   }
 
@@ -26,82 +25,40 @@ class AccountForm extends Component {
     const { value: {toggleCategoryItem} } = this.props
     const { button: {one, two, three, four, five, six, seven, eight, nine, ten} } = this.state
 
-    return(
-      <div align='center'>
-        <h3><p>Users & Accounts</p></h3>
-        <Fab variant="extended" color={one}
-          onClick={() => {
-            toggleCategoryItem('account', 'email_pass');
-            this.toggle(one, 'one');
-          }}>
-          Email/Password Login
-        </Fab>
-        <Fab variant="extended" color={two}
-          onClick={() => {
-            toggleCategoryItem('account', 'facebook');
-            this.toggle(two, 'two');
-          }}>
-          Facebook Login
-        </Fab>
-        <Fab variant="extended" color={three}
-          onClick={() => {
-            toggleCategoryItem('account', 'twitter');
-            this.toggle(three, 'three');
-          }}>
-          Twitter Login
-        </Fab>
-        <br/>
-        <Fab variant="extended" color={four}
-          onClick={() => {
-            toggleCategoryItem('account', 'google');
-            this.toggle(four, 'four');
-          }}>
-          Google Login
-        </Fab>
-        <Fab variant="extended" color={five}
-          onClick={() => {
-            toggleCategoryItem('account', 'linkedin');
-            this.toggle(five, 'five');
-          }}>
-          Linkedin Login
-        </Fab>
-        <Fab variant="extended" color={six}
-          onClick={() => {
-            toggleCategoryItem('account', 'github');
-            this.toggle(six, 'six');
-          }}>
-          GitHub Login
-        </Fab>
-        <Fab variant="extended" color={seven}
-          onClick={() => {
-            toggleCategoryItem('account', 'invitation');
-            this.toggle(seven, 'seven');
-          }}>
-          User Invitation Emails
-        </Fab>
-        <br/>
-        <Fab variant="extended" color={eight}
-          onClick={() => {
-            toggleCategoryItem('account', 'multi_account');
-            this.toggle(eight, 'eight');
-          }}>
-          Multi-tenant Accounts
-        </Fab>
-        <Fab variant="extended" color={nine}
-          onClick={() => {
-            toggleCategoryItem('account', 'subdomain');
-            this.toggle(nine, 'nine');
-          }}>
-          Subdomains
-        </Fab>
-        <Fab variant="extended" color={ten}
-          onClick={() => {
-            toggleCategoryItem('account', 'custom');
-            this.toggle(ten, 'ten');
-          }}>
-          Custom Domains
-        </Fab>
-      </div>
+  return(
+    <div align='center'>
+      <h1>Accounts & Users</h1>
+      <button className={one} onClick={() => { toggleCategoryItem("account", "email_pass"); this.toggle(one, 'one');}}>
+        Email & Password
+      </button>
+      <button className={two} onClick={() => {toggleCategoryItem("account", "facebook"); this.toggle(two, 'two');}}>
+        Facebook
+      </button>
+      <button className={three} onClick={() => {toggleCategoryItem('account', 'twitter'); this.toggle(three, 'three');}}>
+        Twitter
+      </button>
+      <button className={four} onClick={() => {toggleCategoryItem('account', 'google'); this.toggle(four, 'four');}}>
+        Google
+      </button>
+      <button className={five} onClick={() => {toggleCategoryItem('account', 'linkedin'); this.toggle(five, 'five');}}>
+        Linkedin
+      </button>
+      <button className={six} onClick={() => {toggleCategoryItem('account', 'github'); this.toggle(six, 'six');}}>
+        GitHub
+      </button>
+      <button className={seven} onClick={() => {toggleCategoryItem('account', 'invitation'); this.toggle(seven, 'seven');}}>
+        Invitation
+      </button>
+      <button className={eight} onClick={() => {toggleCategoryItem('account', 'multi_account'); this.toggle(eight, 'eight');}}>
+        Multiple Accounts
+      </button>
+      <button className={nine} onClick={() => {toggleCategoryItem('account', 'subdomain'); this.toggle(nine, 'nine');}}>
+        Subdomain
+      </button>
+      <button className={ten} onClick={() => {toggleCategoryItem('account', 'custom'); this.toggle(ten, 'ten');}}>
+        Custom
+      </button>
+    </div>
     )
   }
 }
