676df0ef4b830885e08d27904009adc13dbc65cd
Benjamin Hansen <bhans23@gmail.com>
2020-01-28 20:49:23 -0700

    Side bar changes

--- client/src/components/shared/AboutNav.js
+++ client/src/components/shared/AboutNav.js
@@ -4,30 +4,32 @@ const AboutNav = () => (
 <>   
   <h1>DevPoint</h1>   
   <h1>Studios</h1> 
-  <p>
-    is a web 
+  <div>
+    <p>
+      is a web 
+    </p>
+    <p>
+      development firm 
+    </p>
+    <p>
+      in Salt Lake City 
+    </p>
+    <p>
+      that builds 
+    </p>
+    <p>
+      custom 
+    </p>
+    <p>
+      applications from 
+    </p>
+    <p>  
+      design to 
+    </p> 
+    <p>
+      deployment.
   </p>
-  <p>
-    development firm 
-  </p>
-  <p>
-    in Salt Lake City 
-  </p>
-  <p>
-    that builds 
-  </p>
-  <p>
-    custom 
-  </p>
-  <p>
-    applications from 
-  </p>
-  <p>  
-    design to 
-  </p> 
-  <p>
-    deployment.
-</p>
+</div>
 </>
 )
 export default  AboutNav;
--- client/src/components/shared/Sidebar.js
+++ client/src/components/shared/Sidebar.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
 import Navbar from './Navbar';
-import { Link, Route, Switch } from 'react-router-dom'
-import devpointLogo from '../../images/DevpointLogo.png';
+import DevpointLogo from '../../images/DevpointLogo.png';
 import EstimatorNav from './EstimatorNav';
-import Estimator from './Estimator';
 import AboutNav from './AboutNav';
 import { withRouter } from "react-router-dom";
 
@@ -29,7 +27,7 @@ class Sidebar extends Component{
       <div className="sidebar-clearfix" />
       <div className="sidebar">
         <div className="sidebar-top">
-          <img src={devpointLogo} style={{width: "100%"}} alt="Italian Trulli"></img>
+          <img src={DevpointLogo} style={{width: "100%"}} alt="Italian Trulli"></img>
         </div>
 
         <div className="sidebar-middle">
